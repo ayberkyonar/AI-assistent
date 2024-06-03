0325d1c02b51f86f47549d823972fbf977e02e63
ayberkyonar <yonarayberk@gmail.com>
2024-06-03 11:53:53 +0200

    Project aangemaakt en resolutie gewijzigd in HelloApplication

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.aiassistent {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.aiassistent to javafx.fxml;
+    exports com.example.aiassistent;
+}
