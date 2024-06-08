11a156c920e7959fb40a0d09e3a1762b93d48f27
ayberkyonar <302155034@student.rocmondriaan.nl>
2024-06-08 23:38:17 +0200

    json file aangemaakt en dependency aangemaakt in pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.aiassistent {
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+    requires org.json;
 
     opens com.example.aiassistent to javafx.fxml;
     exports com.example.aiassistent;
